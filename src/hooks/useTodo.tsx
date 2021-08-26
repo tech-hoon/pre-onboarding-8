d096550cca5c1d39111b8ab197b3216614fea5ae
Taekhun <nth9708@naver.com>
2021-08-26 16:25:09 +0900

    Fix[#13]: 아이템 추가/삭제함수 useCallback 추가

--- src/hooks/useTodo.tsx
+++ src/hooks/useTodo.tsx
@@ -1,5 +1,5 @@
 import { TodoTypes } from 'components';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { currentDate } from 'utils/date';
 import mockData from 'utils/data.json';
 
@@ -12,30 +12,23 @@ type useTodoType = {
 
 const useTodo = (): useTodoType => {
   const [items, setItems] = useState<TodoTypes[]>(mockData);
-  const [nextID, setNextID] = useState<number>(items.length);
 
-  const handleTodoCreate = (status: string, text: string, creator: string) => {
-    setItems([
-      ...items,
+  const handleTodoCreate = useCallback((status: string, text: string, creator: string) => {
+    setItems((prevItems) => [
+      ...prevItems,
       {
-        id: nextID + 1,
+        id: prevItems.length + 1,
         taskName: text,
         status: status,
         creator: creator,
         createdAt: currentDate(),
       },
     ]);
-    setNextID((prevID) => prevID + 1);
-  };
+  }, []);
 
-  const handleTodoDelete = (taskID: number) => {
-    const newItems = [...items];
-    newItems.splice(
-      newItems.findIndex(({ id }) => id === taskID),
-      1,
-    );
-    setItems(newItems);
-  };
+  const handleTodoDelete = useCallback((taskID: number) => {
+    setItems((prevItems) => prevItems.filter(({ id }) => id !== taskID));
+  }, []);
 
   const handleTodoUpdate = () => {
     // 드래그앤 드랍 등 todoItem 변경하는 함수
