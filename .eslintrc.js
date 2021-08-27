617e2c5bce73ae2b4f0abd8f25ead1ce7ced5f75
Taekhun <nth9708@naver.com>
2021-08-28 02:34:01 +0900

    Fix[#33]: 불필요한 파일 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
-  rules: { 'react/prop-types': 'off' },
+  rules: { 'react/prop-types': 'off', '@typescript-eslint/no-explicit-any': 'off' },
+
   settings: {
     react: {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
